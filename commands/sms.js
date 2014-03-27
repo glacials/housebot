e8e8c95b43b1ca9146e0dba5df41b11f470cf316
glacials <qhiiyr@gmail.com>
2014-03-26 20:08:54 -0700

    Replace sms link with bombch.us link

--- commands/sms.js
+++ commands/sms.js
@@ -4,7 +4,7 @@ module.exports = function(argv, options) {
     command: 'sms',
     valid: argv[0] === 'sms' && argv.length === 1,
     run: function() {
-      options.bot.say(options.channel, 'https://docs.google.com/spreadsheet/pub?key=0Akik9QOhyPLXdGdmMDdqcy1va3cwZFhYMm02dklqWFE&single=true&gid=8&output=html');
+      options.bot.say(options.channel, 'http://bombch.us/Ut');
     }
   };
 };
