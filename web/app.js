257db25f6e6f52143dcf17ce00fa7f396bb6e655
Ben Carlsson <bc@twitch.tv>
2014-03-28 00:25:09 -0700

    Hmm, let\'s go back to 3000 and use iptables

--- web/app.js
+++ web/app.js
@@ -8,7 +8,7 @@ module.exports = function(devices) {
   var app = express();
 
   // all environments
-  app.set('port', process.env.PORT || 80);
+  app.set('port', process.env.PORT || 3000);
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
   app.use(express.favicon());
