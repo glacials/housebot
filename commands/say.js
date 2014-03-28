891866b6c116da899b0706402c61285cf7b816c6
Ben Carlsson <bc@twitch.tv>
2014-03-27 17:13:49 -0700

    Woops, forgot a vital part of !say

--- commands/say.js
+++ commands/say.js
@@ -1,10 +1,10 @@
 module.exports = function(argv, options) {
   options = options || {};
   return {
-    valid: argv[0] === 'say'
+    valid: argv[0] === 'say' && argv.length > 2,
     run: function() {
       if (options.user.is_owner) {
-        options.bot.say(options.channel, argv.slice(1).join(' '));
+        options.bot.say('#'+argv[1], argv.slice(2).join(' '));
       }
     }
   };
